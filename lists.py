3828b2bf96ee245765cbe0aaacb102390455cee9
febil01 <febil@linuxmail.org>
2021-03-02 21:31:25 +0530

    Added comments and organized the code

--- lists.py
+++ lists.py
@@ -1,20 +1,27 @@
-languages=["C++","Python","Java","C","Javascript","php"] # Assign a list with values
-print('Printing the contents in the list "languages" ',languages) # Printing the list
-print('The length of the list "languages" is ',len(languages)) # printing the length of the list
+# Assign a list with values
+languages=["C++","Python","Java","C","Javascript","php"] 
+ # Printing the list
+print('Printing the contents in the list "languages" ',languages)
+ # printing the length of the list
+print('The length of the list "languages" is ',len(languages))
+# Adding A single item into the list
 languages.append("ruby")
+# Printing the list after appending it 
 print("New list after adding an item using append:",languages)
+# Appending multiple items in the list using extend
 languages.extend(["CSS","xml"])
+# Printing the list after extending it 
 print('New list after multiple items using extend:',languages)
 print('The value of the first item in the list is',languages[0]) # printing a single item in the list using its index or position
 print('The value of the last item in the list is',languages[-1]) # Prints the last element or item in the list 
-print('The value of the second last item in the list is',languages[-2])
-print("THE LAST 3 ITEMS IN THE LIST IS",languages[-1:])
+print('The value of the second last item in the list is',languages[-2]) # Prints the second last item in the list 
 print('The length of the string "languages" is',len(languages))
 languages.append("Keyboard")
 print("Current list:",languages)
 
-if "Keyboard" in languages:
-    languages.remove("Keyboard")
+# using conditional statement to check for a certain condition
+if "Keyboard" in languages: #Checks if keyboard is in the list languages 
+    languages.remove("Keyboard") #Removes the item if it is present 
 print("Updated list",languages)
 
 if "C++" in languages:
@@ -25,36 +32,49 @@ if "C++" not in languages:
 
 healthy=["kale chips","broccoli"]
 backpack=["pizza","frozen custard","apple crisp","kale chips"]
-backpack[:]=[item for item in backpack if item in healthy] #list comprehension 
-power = [(square+1)**2 for square in range(10)]
+# LIST COMPREHENSION
+# Traverse throught backpack and checks if that item is present in another list healthy and stores in backpack
+backpack[:]=[item for item in backpack if item in healthy] 
 print(backpack)
-print("List of first 10 square numbers:",power)
+
+power = [(square+1)**2 for square in range(10)] #Stores the squares of numbers from 1 to 10
+print("List of first 10 square numbers:",power) 
+
 separator=" "
-print(separator.join(map(str,power)))
+print(separator.join(map(str,power))) #Removes the square brackets when printing a list
 print(backpack)
+
 Monitors=["Acer","Acer","Dell","BENQ","Samsung","LG","Sony","Sony"]
 print("Printing the list Monitors:",Monitors)
-print("Sony occurs",Monitors.count("Sony"),'times in the list "Monitors"')
-print("Sony" in Monitors)
-setMonitors=[str(Monitors.count(item)) + " " + item for item in set(Monitors)]
+
+print("Sony occurs",Monitors.count("Sony"),'times in the list "Monitors"') #Finds the occurence of an item in the list
+
+print("Sony" in Monitors) #Prints the number of times an item occurs in the list
+
+setMonitors=[str(Monitors.count(item)) + " " + item for item in set(Monitors)] #Prints the occurence of an item and the display the item in the list
 print(setMonitors)
-lowerMonitors=[]
+
+lowerMonitors=[] #Assigns an empty list
 upperMonitors=[]
 print(Monitors)
 print("Printing List Monitors",Monitors)
 for item in Monitors:
-    lowerMonitors.append(item.lower())
-    upperMonitors.append(item.upper())
+    lowerMonitors.append(item.lower()) #Converting the items in the list to lowercase 
+    upperMonitors.append(item.upper()) #Converting the items in the list to uppercase 
+
 print("List \"Monitors\" in lower case:",lowerMonitors)
 print("List \"Monitors\" in UPPER CASE:",upperMonitors)
+
 Monitors.insert(0,"LG") # To place items in a particular index
 print(Monitors)
 Monitors.remove("Sony")
 print(Monitors)
-del Monitors[0]
+
+del Monitors[0] # To delete an item in the list in a particular index
 print(Monitors)
 Monitors.pop(3)
 print(Monitors)
-del Monitors[Monitors.index("Acer"):Monitors.index("Acer")+2]
+
+del Monitors[Monitors.index("Acer"):Monitors.index("Acer")+2] # To delete items from a range of index
 del Monitors[0:2]
 print(Monitors)
