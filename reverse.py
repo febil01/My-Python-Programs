f843f27e11e554579ae8061756f842907e818997
febil01 <febil@linuxmail.org>
2021-03-09 10:27:18 +0530

    Added a different way to reverse the string

--- reverse.py
+++ reverse.py
@@ -1,7 +1,10 @@
 string=input("Enter a string:")
+#Method 1
 n=len(string) - 1
 reverse=""
 while (n >= 0):
     reverse = reverse + string[n]
     n=n-1
 print("The reversed string is:",reverse)
+#Method 2 (Easier way)
+print("The reversed string is",string[::-1])
