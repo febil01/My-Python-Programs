0996d828af8756eab58604bfe56f237304f9eecd
febil <febil@linuxmail.org>
2021-03-24 11:10:24 +0530

    Maintains the order of the list

--- duplicates.py
+++ duplicates.py
@@ -2,7 +2,8 @@ from collections import Counter
 duplicates=["Mango","Grapes","Watermelon","Oranges","Pears","Strawberry","Kiwi","Mango","Pears","Oranges","Oranges","Strawberry"]
 print("List before removing duplicates:",duplicates)
 print(Counter(duplicates))
+duplicates[:]=duplicates[::-1] #To maintain the order of the list
 for item in duplicates:
     if duplicates.count(item) > 1:
         duplicates.remove(item)
-print("List after removing duplicates:",duplicates)
+print("List after removing duplicates:",duplicates[::-1]) #Printing the correct order of the list
