f141d875e77cde1664cb6c938bf85931ea036be3
febil <febil@linuxmail.org>
2021-03-30 13:26:41 +0530

    added another method to swap

--- swap.py
+++ swap.py
@@ -3,3 +3,12 @@ swap2="bye"
 print("Values before swapping:",swap1,swap2)
 swap1,swap2=swap2,swap1
 print("Values after swapping:",swap1,swap2)
+
+#method 2 
+a=10
+b=20
+print("Values before swapping:",a,b)
+temp=a
+a=b
+b=temp
+print("Values after swapping:",a,b)
