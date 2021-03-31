57f47cae4a58927df6d2c5f769f66174822d4a7e
febil <febil@linuxmail.org>
2021-03-31 21:37:45 +0530

    More updates

--- listSlice.py
+++ listSlice.py
@@ -7,3 +7,7 @@ print("Printing all the even elements in the list:",Computer[1::2])
 print("Printing all the odd elements in the list:",Computer[::2])
 print("Printing all the even elements in the list in reverse order:",Computer[-2::-2])
 print("Printing all the odd elements in the list in reverse order:",Computer[::-2])
+print("Printing the first 3 elements in the list:",Computer[:3:1])
+print("Printing the last 3 elements in the list:",Computer[:-4:-1])
+print(Computer[-1:-4:]) #list will be empty since Index jump by default is positive 
+print(Computer[::4])
