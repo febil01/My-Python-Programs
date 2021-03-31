e6e175d42c38a0db43cf6f094c8b3b66f7adca86
febil <febil@linuxmail.org>
2021-03-31 21:16:20 +0530

    Initial commit

---
+++ listSlice.py
@@ -0,0 +1,9 @@
+Computer=["Monitor","Keyboard","CPU","SSD","HDD","GPU","RAM"] #Initialize list with elements
+print("The contents of the list:",Computer)
+print("The contents of the list in reverse order:",Computer[::-1])
+print()
+print("The first two elements in the list:",Computer[0:2:1])
+print("Printing all the even elements in the list:",Computer[1::2])
+print("Printing all the odd elements in the list:",Computer[::2])
+print("Printing all the even elements in the list in reverse order:",Computer[-2::-2])
+print("Printing all the odd elements in the list in reverse order:",Computer[::-2])
