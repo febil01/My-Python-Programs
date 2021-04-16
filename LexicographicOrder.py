5896876511d74d7055a19d484d61f22cbcb95efa
febil <febil@linuxmail.org>
2021-04-16 16:09:55 +0530

    Initial commit

---
+++ LexicographicOrder.py
@@ -0,0 +1,4 @@
+numbers=[1,-11,231,121212,345,1111,4563,121,357,11,563,324,1,-12]
+print("Printing the original list",numbers)
+print("Sorting the set of numbers based on value:",sorted(numbers))
+print("Sorting the set of numbers in lexicographic order:",sorted(numbers,key=str))
