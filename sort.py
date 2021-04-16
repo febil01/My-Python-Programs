b5c8724c9b9ecf0ad14c1a92d66533bd77f3c552
febil <febil@linuxmail.org>
2021-04-16 15:18:52 +0530

    Added another method to sort the list

--- sort.py
+++ sort.py
@@ -1,4 +1,7 @@
 strings= ["1","0","fire","ICE","#","/"]
 print("Printing the original list:",strings) 
-strings.sort() #Sorts list based on ASCII 
+strings.sort() #Sorts list based on ASCII and modifies the list 
 print("List after sorting:",strings)
+numbers=[10,2,3,5,67,12]
+print("Printing the original list:",numbers)
+print(sorted(numbers)) #Sorts list without modifying it
