6c537854ebc82ba1aa778d4d44a04154b83bb638
febil <febil@linuxmail.org>
2021-04-16 14:51:17 +0530

    Added another method to reverse the list

--- reverselist.py
+++ reverselist.py
@@ -1,4 +1,10 @@
-data=[1,2,3,4,5,6]
-print("Value of the list \"data\":",data)
-data.reverse()
-print("Value of the reversed list:",data)
+data1=[1,2,3,4,5,6]
+print("The value of list 1:",data1)
+data1.reverse()
+print("The value of list 1 in reverse:",data1)
+#Method 2
+data2=["Chrome","firefox","safari","opera","brave"]
+print("The value of list 2:",data2)
+for item in range(len(data2)//2):
+    data2[item],data2[-item-1] = data2[-item-1], data2[item]
+print("The value of list 2 in reverse:",data2)
