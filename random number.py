473d9cd98cb34168961fc0fe0165ada2bcd98c41
febil01 <febil@linuxmail.org>
2021-05-09 10:34:43 +0530

    Initial commit

---
+++ random number.py
@@ -0,0 +1,12 @@
+import random
+print("Generating 5 random numbers")
+for i in range(5):
+    print(random.randint(0,5))
+
+"""
+or 
+from random import randint 
+print("Generating 5 random numbers")
+for i in range(5):
+    print(randint(0,5))
+"""
